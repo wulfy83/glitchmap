2f73f478828c1347de6f36988d759ece62cf535e
Wulfy <wulf38@gmail.com>
2021-06-10 18:48:35 -0400

    Add more points

--- data.js
+++ data.js
@@ -24,7 +24,7 @@ POINTS = [
 
             =foothills_clip_2
             To jump directly to the lower cave:
-            - get in the corner and buffer down-left to jump
+            - quick hop down
             - hold down to jump again
 
             =foothills_clip_3
@@ -35,9 +35,30 @@ POINTS = [
     },
 
     {
+        title: "New DMA",
+        world: "light",
+        pos: [3516, 2629],
+        text: `
+            =new_dma_1
+            - go here or 1-2 pixels lower
+            - buffer or spinspeed into the corner until jump
+            - press down to transition
+
+            =new_dma_2
+            - jump right
+            - move right past the brown
+            - hold up-left to jump
+            - tap right
+            - quick hop down
+            - hold down to jump again
+            - hold up to enter cave
+        `,
+    },
+
+    {
         title: "Classic DMA",
         world: "light",
-        pos: [3532, 2613],
+        pos: [3780, 2629],
         text: `
             =classic_dma_1
             - up teleport
@@ -51,12 +72,36 @@ POINTS = [
     },
 
     {
+        title: "Ether DMD",
+        world: "light",
+        pos: [4016, 387],
+        text: `
+            =ether_dmd
+            - clip through the corner
+            - dash-turn left
+        `,
+    },
+
+    {
+        title: "Light DMD",
+        world: "light",
+        pos: [4339, 2435],
+        text: `
+            =light_dmd
+            - superspeed clip through left corner
+            - tap right
+            - dash-turn up and bonk
+            - transition down
+        `,
+    },
+
+    {
         title: "Hera Wrap (Spinspeed)",
         world: "light",
         pos: [5452, 2452],
         text: `
             =hera_spinspeed
-            - superspeed clip through right corner
+            - clip through right corner
             - tap up
             - down 1 tile, right 2 tiles, then up
         `,
@@ -73,6 +118,199 @@ POINTS = [
             - hold left then up
         `,
     },
+
+    {
+        title: "Spectacle Rock Teleport",
+        world: "light",
+        pos: [5161, 726],
+        text: `
+            =spec_rock_teleport
+            - teleport down
+        `,
+    },
+
+    {
+        title: "Death Mountain Bomb Clip",
+        world: "light",
+        pos: [5435, 710],
+        text: `
+            =light_dm_bomb_clip_1
+            =light_dm_bomb_clip_2
+            - place bomb
+            - dash into the bomb as it explodes
+            - dash
+            - tap down
+        `,
+    },
+
+    {
+        title: "Death Mountain Land Bridge Teleport",
+        world: "light",
+        pos: [6016, 871],
+        text: `
+            =dm_land_bridge_teleport
+            - teleport down
+            - tap down, tap left, half circle left, repeat until free
+            - jump right and transition
+        `,
+    },
+
+    {
+        title: "Death Mountain Land Bridge Return",
+        world: "light",
+        pos: [7371, 710],
+        text: `
+            =dm_land_bridge_return
+            - clip left through the corner
+            - stay high for transition
+        `,
+    },
+
+    {
+        title: "Death Mountain Cloud Teleport (West)",
+        world: "light",
+        pos: [6355, 387],
+        text: `
+            =cloud_teleport_west
+            - teleport up
+        `,
+    },
+
+    {
+        title: "Death Mountain Cloud Teleport (East)",
+        world: "light",
+        pos: [7742, 677],
+        text: `
+            =cloud_teleport_east
+            - teleport up
+        `,
+    },
+
+    {
+        title: "Floating Island Clip",
+        world: "light",
+        pos: [7887, 65],
+        text: `
+            =floating_island_noflips
+            No flippers:
+            - jump down into water as far right as possible
+            - sword buffer left 1, then down 1
+            - dash right
+
+            =floating_island_flips
+            Flippers:
+            - aim for the corner and jump
+            - hold right and pump down
+        `,
+    },
+
+    {
+        title: "Turtle Rock Teleport (Light)",
+        world: "light",
+        pos: [9226, 435],
+        text: `
+            =light_tr_teleport
+            - teleport down
+        `,
+    },
+
+    {
+        title: "Waterfall DMD",
+        world: "light",
+        pos: [9032, 984],
+        text: `
+            =waterfall_dmd_1
+            - clip left through the corner
+
+            =waterfall_dmd_2
+            =waterfall_dmd_3
+            - transition down here
+            - tap right, tap down
+        `,
+    },
+
+    {
+        title: "Zora DMD",
+        world: "light",
+        pos: [9548, 1081],
+        text: `
+            =zora_dmd
+            - clip left through the corner
+            - hold right
+        `,
+    },
+
+    {
+        title: "Witch DMD",
+        world: "light",
+        pos: [7823, 2371],
+        text: `
+            =witch_dmd
+            - buffer into the corner this much
+            - dash-turn down
+            - press up to transition
+        `,
+    },
+
+    {
+        title: "Zora Ledge Clip",
+        world: "light",
+        pos: [9855, 1242],
+        text: `
+            =zora_ledge_clip
+            - buffer towards this bump until you jump
+            - go all the way around
+        `,
+    },
+
+    {
+        title: "Zora DMA",
+        world: "light",
+        pos: [9597, 1435],
+        text: `
+            =zora_dma_1
+            - teleport up
+            - tap right
+            - carefully move up-left to the ledge
+
+            =zora_dma_2
+            - jump left
+            - transition up
+
+            =zora_dma_3
+            - transition left
+            - jump down
+            - tap left
+
+            =zora_dma_4
+            For bottom of paradox:
+            - move up 1 tile
+            - dash turn left
+
+            For top of spiral cave:
+            - move up 1 tile
+            - transition left and auto run
+            - move up 2 tiles
+            - dash right and stop before ledge
+            - move up to ledge
+            - jump right
+            - move right spiral cave drop and jump down
+        `,
+    },
+
+    {
+        title: "Agahnim's Tower River Clip",
+        world: "light",
+        pos: [3613, 3629],
+        text: `
+            =aga_river_clip
+            - clip into the wall around here
+            - dash turn down to jump
+            - dash right and find the screen transition
+            - up left into aga tower
+        `,
+    },
+
 
     {
         title: "Dark Lumberjack DMA",
@@ -107,6 +345,53 @@ POINTS = [
             To get from the upper cave to the lower cave:
             - clip into the bump
             - hold down
+        `,
+    },
+
+    {
+        title: "Dark DMD",
+        world: "dark",
+        pos: [4339, 2435],
+        text: `
+            =dark_dmd
+            - clip through left corner
+            - dash-turn up and bonk
+            - transition down
+        `,
+    },
+
+    {
+        title: "Dark Death Mountain Bomb Clip",
+        world: "dark",
+        pos: [5435, 710],
+        text: `
+            =dark_dm_bomb_clip_1
+            =dark_dm_bomb_clip_2
+            - move left to the wall and place bomb
+            - dash into the bomb as it explodes
+            - dash
+            - tap down
+        `,
+    },
+
+    {
+        title: "Dark Death Mountain Land Bridge Return",
+        world: "dark",
+        pos: [7371, 710],
+        text: `
+            =dark_dm_land_bridge_return
+            - clip left through the corner
+            - stay high for transition
+        `,
+    },
+
+    {
+        title: "Turtle Rock Teleport (Dark)",
+        world: "dark",
+        pos: [9226, 435],
+        text: `
+            =dark_tr_teleport
+            - teleport down
         `,
     },
 ];
