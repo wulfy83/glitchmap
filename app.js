a984cc8a7e7f970616bbf448a1e1d3ccf298550b
Wulfy <wulf38@gmail.com>
2021-06-09 23:47:16 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+const MAP_SIZE = 620;
+
+const light_map = document.getElementById("light-map");
+const dark_map = document.getElementById("dark-map");
+
+function event_coordinates(event) {
+    const rect = event.target.getBoundingClientRect();
+    return [
+        event.pageX - rect.left,
+        event.pageY - rect.top,
+    ];
+}
+
+function relative_coordinates(coordinates) {
+    return coordinates.map(x => Math.round((x / MAP_SIZE) * 10000));
+}
+
+function absolute_coordinates(coordinates) {
+    return coordinates.map(x => Math.round((x / 10000) * MAP_SIZE));
+}
+
+function log_coordinates(coordinates) {
+    console.log("[" + coordinates[0] + ", " + coordinates[1] + "]");
+}
+
+function open_info_box(point) {
+    close_info_box();
+    const parent = (point.world === "light") ? dark_map : light_map;
+    const info_box = document.createElement("div");
+    info_box.id = "info-box";
+    info_box.onclick = function (event) {
+        event.stopPropagation();
+    };
+    info_box.innerHTML = format_point_text(point);
+    parent.appendChild(info_box);
+}
+
+function close_info_box() {
+    const info_box = document.getElementById("info-box");
+    if (info_box !== null) {
+        info_box.remove();
+    }
+}
+
+function format_point_text(point) {
+    const title = `<div class="info-box-title">${point.title}</div>`;
+    const text = point.text.trim()
+        .replace(/(\r\n|\r|\n)/g, '<br>')
+        .replace(/(=([A-Za-z0-9_]+))/g, (str, g1, g2) => `<img class="screenshot" src="screenshots/${g2}.png">`);
+    return title + text;
+}
+
+function init_world(world) {
+    world.map.style.width = MAP_SIZE + "px";
+    world.map.style.height = MAP_SIZE + "px";
+    world.map.onclick = function (event) {
+        coordinates = relative_coordinates(event_coordinates(event));
+        log_coordinates(coordinates);
+    }
+}
+
+function main() {
+    const worlds = [
+        { name: "light", map: light_map },
+        { name: "dark",  map: dark_map  },
+    ];
+
+    for (const world of worlds) {
+        init_world(world);
+        const world_points = POINTS.filter(p => p.world == world.name);
+        for (const point of world_points) {
+            const [x, y] = absolute_coordinates(point.pos);
+            const element = document.createElement("div");
+            element.classList.add("point");
+            element.style.left = x + "px";
+            element.style.top = y + "px";
+            element.onclick = function (event) {
+                open_info_box(point);
+                event.stopPropagation();
+            };
+            world.map.appendChild(element);
+        }
+    }
+}
+
+main();
---
+++ data.js
@@ -0,0 +1,112 @@
+POINTS = [
+    {
+        title: "Lumberjack DMA",
+        world: "light",
+        pos: [3581, 629],
+        text: `
+            =lumberjack_dma_1
+            - down teleport and hold
+            - jump right
+
+            =lumberjack_dma_2
+            - dashturn tight to get in bounds
+        `,
+    },
+
+    {
+        title: "Foothills Clip",
+        world: "light",
+        pos: [3290, 1371],
+        text: `
+            =foothills_clip_1
+            - down teleport and hold
+            - jump right
+
+            =foothills_clip_2
+            To jump directly to the lower cave:
+            - get in the corner and buffer down-left to jump
+            - hold down to jump again
+
+            =foothills_clip_3
+            To get from the upper cave to the lower cave:
+            - clip into the bump
+            - hold down
+        `,
+    },
+
+    {
+        title: "Classic DMA",
+        world: "light",
+        pos: [3532, 2613],
+        text: `
+            =classic_dma_1
+            - up teleport
+
+            =classic_dma_2
+            - transition right here
+
+            =classic_dma_3
+            - wiggle up and down until the entrance is on screen then enter
+        `,
+    },
+
+    {
+        title: "Hera Wrap (Spinspeed)",
+        world: "light",
+        pos: [5452, 2452],
+        text: `
+            =hera_spinspeed
+            - superspeed clip through right corner
+            - tap up
+            - down 1 tile, right 2 tiles, then up
+        `,
+    },
+
+    {
+        title: "Hera Wrap (Buffers)",
+        world: "light",
+        pos: [5694, 2339],
+        text: `
+            =hera_buffer
+            - buffer left through the bump
+            - tap right
+            - hold left then up
+        `,
+    },
+
+    {
+        title: "Dark Lumberjack DMA",
+        world: "dark",
+        pos: [3581, 629],
+        text: `
+            =dark_lumberjack_dma_1
+            - down teleport and hold
+            - jump right
+            - tap down after transition
+
+            =dark_lumberjack_dma_2
+            - jump down in bounds
+        `,
+    },
+
+    {
+        title: "Bumper Cave Clip",
+        world: "dark",
+        pos: [3290, 1371],
+        text: `
+            =bumper_clip_1
+            - down teleport and hold
+            - jump right
+
+            =bumper_clip_2
+            To jump directly to the lower cave:
+            - get in the corner and buffer down-left to jump
+            - hold down to jump again
+
+            =bumper_clip_3
+            To get from the upper cave to the lower cave:
+            - clip into the bump
+            - hold down
+        `,
+    },
+];
