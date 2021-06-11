06aacd639eda6786ef9cc388d1af9eb8157dfe50
Wulfy <wulf38@gmail.com>
2021-06-11 00:46:20 -0400

    Add more points (2)

--- data.js
+++ data.js
@@ -96,6 +96,52 @@ POINTS = [
     },
 
     {
+        title: "Graveyard Ledge Clip",
+        world: "light",
+        pos: [5694, 2677],
+        text: `
+            =graveyard_ledge_clip
+            With spinspeed:
+            - clip left through the corner
+            - hold up-left while falling
+            - hold up to transition
+
+            No spinspeed:
+            - buffer into the corner 3 times
+            - dash turn left
+            - hold up-right while falling
+            - hold up to transition
+        `,
+    },
+
+    {
+        title: "King's Tomb Clip",
+        world: "light",
+        pos: [6000, 2935],
+        text: `
+            =kings_tomb
+            - graveyard ledge clip
+            - buffer up 1 frame
+            - dash turn right
+            - tap up, left, down
+            - lift rock
+        `,
+    },
+
+    {
+        title: "East DM Clip",
+        world: "light",
+        pos: [6613, 2065],
+        text: `
+            =east_dm_clip_1
+            - clip right through the corner
+
+            =east_dm_clip_2
+            - jump down to return in bounds
+        `,
+    },
+
+    {
         title: "Hera Wrap (Spinspeed)",
         world: "light",
         pos: [5452, 2452],
@@ -158,11 +204,21 @@ POINTS = [
     {
         title: "Death Mountain Land Bridge Return",
         world: "light",
-        pos: [7371, 710],
+        pos: [7145, 887],
         text: `
             =dm_land_bridge_return
             - clip left through the corner
             - stay high for transition
+        `,
+    },
+
+    {
+        title: "Death Mountain Peg Skip",
+        world: "light",
+        pos: [7339, 694],
+        text: `
+            =dm_peg_skip
+            - teleport up
         `,
     },
 
@@ -311,6 +367,195 @@ POINTS = [
         `,
     },
 
+    {
+        title: "Castle Jump (requires flippers)",
+        world: "light",
+        pos: [3855, 4435],
+        text: `
+            =castle_jump
+            - stand with your shadow slightly above the wall shadow
+            - clip into the wall until blocked
+            - dash up
+            - tap down
+            - hold right to jump
+        `,
+    },
+
+    {
+        title: "Witch to Eastern (West)",
+        world: "light",
+        pos: [7371, 3548],
+        text: `
+            =witch_to_eastern_west
+            - teleport down to jump
+            - tap up, left
+            - walk right 2 tiles
+            - transition down
+        `,
+    },
+
+    {
+        title: "Witch to Eastern (East)",
+        world: "light",
+        pos: [8129, 3532],
+        text: `
+            =witch_to_eastern_east
+            - teleport down to jump
+            - jump down
+            - transition down
+        `,
+    },
+
+    {
+        title: "Witch to Zora",
+        world: "light",
+        pos: [8581, 3484],
+        text: `
+            =witch_to_zora
+            - teleport up to jump
+            - transition right
+        `,
+    },
+
+    {
+        title: "Zora to Witch",
+        world: "light",
+        pos: [9194, 2984],
+        text: `
+            =zora_to_witch
+            - teleport down to jump
+            - transition left
+        `,
+    },
+
+    {
+        title: "Bat Cave Clip",
+        world: "light",
+        pos: [2581, 5629],
+        text: `
+            =bat_cave_clip
+            - align with the fence
+            - clip partially into the slope
+            - dash to jump, hold right while falling
+            - tap up
+            - swim right to the wall
+            - jump up out of the water
+            - jump down into the hole
+        `,
+    },
+
+    {
+        title: "Kakariko to Race Game",
+        world: "light",
+        pos: [242, 6081],
+        text: `
+            =kak_to_race_1
+            - teleport down to jump
+            - tap right
+            - move up left slightly then transition down
+
+            =kak_to_race_2
+            To get the race item:
+            - jump down here
+
+            =kak_to_race_3
+            To reach desert:
+            - sword buffer carefully to reach the lowest ledge
+            - slow hop down twice
+
+            =kak_to_race_4
+            To reach eastern:
+            - transition left on the ledge
+            - move into the right side of the tree and walk up into eastern
+        `,
+    },
+
+    {
+        title: "Race Game from South",
+        world: "light",
+        pos: [274, 7032],
+        text: `
+            =race_game_south_1
+            - buffer to down-teleport location but 1 pixel higher
+            - dash turn right
+            - buffer down left to get item
+            - tap right to escape
+
+            =race_game_south_2
+            To continue to desert:
+            - teleport up to jump
+            - move left 3 tiles and transition down
+        `,
+    },
+
+    {
+        title: "Race Game to Desert",
+        world: "light",
+        pos: [274, 7323],
+        text: `
+            =race_game_to_desert_1
+            - teleport up to jump
+
+            =race_game_to_desert_2
+            To directly enter desert back:
+            - walk here and transition down
+            - hold up to jump down
+        `,
+    },
+
+    {
+        title: "Library Screen to Checkerboard Cave",
+        world: "light",
+        pos: [1984, 7387],
+        text: `
+            =library_to_checker
+            - teleport down and hold down-left to jump
+        `,
+    },
+
+    {
+        title: "South of Grove to Checkerboard Cave",
+        world: "light",
+        pos: [2726, 7919],
+        text: `
+            =grove_to_checker
+            - teleport down and hold down-left to jump
+        `,
+    },
+
+    {
+        title: "Cave 45 Clip",
+        world: "light",
+        pos: [3129, 8258],
+        text: `
+            =cave_45_clip
+            - teleport down
+            - tap right
+        `,
+    },
+
+    {
+        title: "Cave 45 to Checkerboard",
+        world: "light",
+        pos: [2597, 8532],
+        text: `
+            =cave_45_to_checker
+            - buffer into the bump until jump
+            - transition left
+        `,
+    },
+
+    {
+        title: "Eastern to Zora",
+        world: "light",
+        pos: [8790, 4048],
+        text: `
+            =eastern_to_zora
+            - teleport up
+            - move left to edge
+            - dash up
+        `,
+    },
 
     {
         title: "Dark Lumberjack DMA",
@@ -392,6 +637,93 @@ POINTS = [
         text: `
             =dark_tr_teleport
             - teleport down
+        `,
+    },
+
+    {
+        title: "Dark Witch to PoD (West)",
+        world: "dark",
+        pos: [7371, 3548],
+        text: `
+            =dark_witch_to_pod_west
+            - teleport down to jump
+            - tap up, left
+            - walk right 2 tiles
+            - transition down
+        `,
+    },
+
+    {
+        title: "Dark Witch to PoD (East)",
+        world: "dark",
+        pos: [8129, 3532],
+        text: `
+            =dark_witch_to_pod_east
+            - teleport down to jump
+            - tap left, up
+            - transition down to the left
+            - tap left, up
+        `,
+    },
+
+    {
+        title: "Dark Witch to Catfish",
+        world: "dark",
+        pos: [8581, 3484],
+        text: `
+            =dark_witch_to_catfish
+            - teleport up to jump
+            - transition right
+        `,
+    },
+
+    {
+        title: "Catfish to Dark Witch or PoD",
+        world: "dark",
+        pos: [9194, 2984],
+        text: `
+            =catfish_to_dark_witch
+            - teleport down to jump
+            - transition left
+
+            =catfish_to_pod
+            To reach PoD:
+            - teleport down
+            - dash down to transition
+        `,
+    },
+
+    {
+        title: "Village of Outcasts to Mire",
+        world: "dark",
+        pos: [242, 6081],
+        text: `
+            =voo_to_mire_1
+            - teleport down to jump
+            - tap right
+            - move up left slightly then transition down
+
+            =voo_to_mire_2
+            - carefully walk this far left
+            - slow hop down twice
+        `,
+    },
+
+    {
+        title: "Pyramid Fairy Offset Portal",
+        world: "dark",
+        pos: [4677, 4855],
+        text: `
+            =pyramid_fairy_1
+            - hera wrap and jump down to the portal
+            - climb down the mountain
+
+            =pyramid_fairy_2
+            - stand here and mirror
+            - die or flute away
+
+            =pyramid_fairy_3
+            - walk up into the portal
         `,
     },
 ];
